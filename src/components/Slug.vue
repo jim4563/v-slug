7b54c7d6541dde880821389d9597ccc379e19712
Dmytro Rybachek <Puss.in.Boots@i.ua>
2021-11-14 00:04:01 +0200

    push lib

--- src/components/Slug.vue
+++ src/components/Slug.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>Hello word 2 @com</h1>
+  <h1>
+    Hello word 2 @com
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid amet commodi dicta distinctio et eveniet exercitationem facere fuga, impedit modi nostrum nulla perspiciatis recusandae unde ut veritatis voluptate. Ipsa?
+  </h1>
 </template>
 
 <script>
